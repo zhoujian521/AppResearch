b65c061afbdae46e485861a9fd0c975e22ebda68
zhoujian <jian.zhou@litex.io>
2018-12-12 12:48:29 +0800

    使用 etherscan-api 查询交易列表

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
     const rawurl = 'https://mainnet.infura.io';
     GethModel.init({rawurl});
   }
-
+  
   // 0x71c7656ec7ab88b098defb751b7401b5f6d8976f
   // 0xb5538753F2641A83409D2786790b42aC857C5340
   _getBalance =()=>{
@@ -28,10 +28,31 @@ export default class App extends Component {
     GethModel.newWallet();
   }
 
+
+  _etherscanGetBalance= async ()=>{
+    const apiKey = 'RCDFTNQSB3PW6FMAS4HVQCTD9EYQ1TPMTS';
+    const api = require('etherscan-api').init(apiKey,'rinkeby', 1000);
+    const balance = await api.account.txlist('0x38bCc5B8b793F544d86a94bd2AE94196567b865c');
+    const params = {
+      address:'0x38bCc5B8b793F544d86a94bd2AE94196567b865c',
+      startblock:0,
+      endblock:999999999,
+      page:1,
+      offset:20,
+      sort:'desc'
+    };
+    const {address, startblock, endblock, page,  offset, sort} = params;
+    const txlist = await api.account.txlist(address, startblock, endblock, page, offset, sort);
+    console.log('============txlist========================');
+    console.log(txlist);
+    console.log('============txlist========================');
+
+
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={()=>this._init()}>
+    const iOSGeth = <View>
+      <TouchableOpacity onPress={()=>this._init()}>
           <View style={[styles.button, {marginTop: 80}]}>
             <Text>init 【https://mainnet.infura.io】</Text>
           </View>
@@ -52,6 +73,18 @@ export default class App extends Component {
             <Text>doSomethingExpensive</Text>
           </View>
         </TouchableOpacity>
+    </View>
+
+    return (
+      <View style={styles.container}>
+        {iOSGeth}
+        <TouchableOpacity onPress={()=>this._etherscanGetBalance()}>
+          <View style={styles.button}>
+            <Text>Etherscan=>GetBalance</Text>
+          </View>
+        </TouchableOpacity>
+
+
       </View>
     );
   }
